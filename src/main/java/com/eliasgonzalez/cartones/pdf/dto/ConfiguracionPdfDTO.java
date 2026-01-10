5219613794aba882b46e630737c258d69454aa3c
Elias Gonzalez <elias.gonzalez@ncoders.solutions>
2026-01-10 12:14:12 -0300

    se agregan validaciones a ConfiguracionPdfDTO

--- src/main/java/com/eliasgonzalez/cartones/pdf/dto/ConfiguracionPdfDTO.java
+++ src/main/java/com/eliasgonzalez/cartones/pdf/dto/ConfiguracionPdfDTO.java
@@ -1,19 +1,55 @@
 package com.eliasgonzalez.cartones.pdf.dto;
 
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
 import java.util.List;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter @Setter
-@Builder
+@Getter
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.ANY)
 public class ConfiguracionPdfDTO {
 
-    private Integer InicioSeneteGral;
-    private Integer InicioTelebingoGral;
-    private LocalDate fechaSorteo;
-    private List<RangoCortadoDTO> rangosCortados;
+    private Integer inicioSeneteGral;
+    private Integer inicioTelebingoGral;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate fechaSorteo = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
+
+    private List<RangoCortadoDTO> rangosCortados = new ArrayList<>();
+
+    public ConfiguracionPdfDTO() {
+    }
+
+    public ConfiguracionPdfDTO(Integer inicioSeneteGral, Integer inicioTelebingoGral, LocalDate fechaSorteo, List<RangoCortadoDTO> rangosCortados) {
+        setInicioSeneteGral(inicioSeneteGral);
+        setInicioTelebingoGral(inicioTelebingoGral);
+        setFechaSorteo(fechaSorteo);
+        setRangosCortados(rangosCortados);
+    }
+
+    @JsonProperty("inicioSenete")
+    public void setInicioSeneteGral(Integer inicioSeneteGral) {
+        this.inicioSeneteGral = inicioSeneteGral == null ? 0 : inicioSeneteGral;
+    }
+
+    @JsonProperty("inicioTelebingo")
+    public void setInicioTelebingoGral(Integer inicioTelebingoGral) {
+        this.inicioTelebingoGral = inicioTelebingoGral == null ? 0 : inicioTelebingoGral;
+    }
+
+    @JsonProperty("fechaSorteo")
+    public void setFechaSorteo(LocalDate fechaSorteo) {
+        this.fechaSorteo = fechaSorteo == null ? LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.SUNDAY)) : fechaSorteo;
+    }
+
+    @JsonProperty("rangosCortados")
+    public void setRangosCortados(List<RangoCortadoDTO> rangosCortados) {
+        this.rangosCortados = rangosCortados == null ? new ArrayList<>() : rangosCortados;
+    }
 }
