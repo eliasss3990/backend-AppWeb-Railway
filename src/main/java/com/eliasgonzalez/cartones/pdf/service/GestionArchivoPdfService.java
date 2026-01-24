c3eb00e038a8d79eb2ef358c4fdde4c2ab424436
Elias Gonzalez <elias.gonzalez@ncoders.solutions>
2026-01-24 18:56:28 -0300

    se elimina try-catch innecesario

--- src/main/java/com/eliasgonzalez/cartones/pdf/service/GestionArchivoPdfService.java
+++ src/main/java/com/eliasgonzalez/cartones/pdf/service/GestionArchivoPdfService.java
@@ -4,14 +4,10 @@ import com.eliasgonzalez.cartones.pdf.component.SaveInMemoryTemp;
 import com.eliasgonzalez.cartones.pdf.entity.PdfProcesos;
 import com.eliasgonzalez.cartones.pdf.interfaces.IPdfService;
 import com.eliasgonzalez.cartones.pdf.interfaces.PdfProcesosRepository;
-import com.eliasgonzalez.cartones.shared.exception.UnprocessableEntityException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.io.IOException;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -26,21 +22,17 @@ public class GestionArchivoPdfService {
     public Resource generarPaqueteZip(String procesoId) {
         PdfProcesos proceso = gestionDistribucionService.buscarProcesoOError(procesoId);
 
-        try {
-            Resource zip = pdfService.obtenerZipPdfs(
-                    procesoId,
-                    saveInMemoryTemp.getVendedorSimuladoDTOs(),
-                    saveInMemoryTemp.getFechaSorteoSenete(),
-                    saveInMemoryTemp.getFechaSorteoTelebingo()
-            );
+        Resource zip = pdfService.obtenerZipPdfs(
+                procesoId,
+                saveInMemoryTemp.getVendedorSimuladoDTOs(),
+                saveInMemoryTemp.getFechaSorteoSenete(),
+                saveInMemoryTemp.getFechaSorteoTelebingo()
+        );
 
-            // Finalizar proceso
-            ProcesoIdService.VerificandoToCompletado(procesoId, proceso);
-            pdfProcesosRepo.save(proceso);
+        // Finalizar proceso
+        ProcesoIdService.VerificandoToCompletado(procesoId, proceso);
+        pdfProcesosRepo.save(proceso);
 
-            return zip;
-        } catch (IOException e) {
-            throw new UnprocessableEntityException("Error al generar el archivo comprimido de etiquetas.", List.of(e.getMessage()));
-        }
+        return zip;
     }
 }
