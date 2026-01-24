ff84f0844a645c4834632a1e95f4c7e00e0bd520
Elias Gonzalez <elias.gonzalez@ncoders.solutions>
2026-01-24 18:08:22 -0300

    se elimina metodo sin uso

--- src/main/java/com/eliasgonzalez/cartones/pdf/dto/RangoCortadoDTO.java
+++ src/main/java/com/eliasgonzalez/cartones/pdf/dto/RangoCortadoDTO.java
@@ -11,8 +11,4 @@ public class RangoCortadoDTO {
     private int inicio;
     private int fin;
 
-    // Validación lógica simple
-    public boolean isValido() {
-        return inicio > 0 && fin > 0 && inicio <= fin;
-    }
 }
