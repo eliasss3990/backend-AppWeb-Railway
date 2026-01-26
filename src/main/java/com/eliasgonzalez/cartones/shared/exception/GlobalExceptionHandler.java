04050b6675e803b654531bcb47fca886b8e7f87c
Elias Gonzalez <elias.gonzalez@ncoders.solutions>
2026-01-26 08:59:07 -0300

    se añaden logs de error en el GlobalExceptionHandler, y tambien se elimina List.of(ex.getMessage()) del atrapador de excepciones globales

--- src/main/java/com/eliasgonzalez/cartones/shared/exception/GlobalExceptionHandler.java
+++ src/main/java/com/eliasgonzalez/cartones/shared/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.eliasgonzalez.cartones.shared.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,11 +11,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.List;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     // Manejador específico para las posibles excepciones de Excel
     @ExceptionHandler(ExcelProcessingException.class)
     public ResponseEntity<ErrorResponse> handleExcelProcessingException(ExcelProcessingException ex) {
+
+        log.error("SE LANZÓ 'handleExcelProcessingException': {}", ex.getMessage());
 
         HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
 
@@ -33,6 +37,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(FileProcessingException.class)
     public ResponseEntity<ErrorResponse> handleFileProcessingException(FileProcessingException ex){
 
+        log.error("SE LANZÓ 'handleFileProcessingException': {}", ex.getMessage());
+
         HttpStatus status = HttpStatus.UNSUPPORTED_MEDIA_TYPE;
 
         ErrorResponse response = ErrorResponse
@@ -49,6 +55,8 @@ public class GlobalExceptionHandler {
     // Manejador específico para las posibles excepciones para PdfCreationException
     @ExceptionHandler(PdfCreationException.class)
     public ResponseEntity<ErrorResponse> handlePdfCreationException(PdfCreationException ex){
+
+        log.error("SE LANZÓ 'handlePdfCreationException': {}", ex.getMessage());
 
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
@@ -67,6 +75,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UnprocessableEntityException.class)
     public ResponseEntity<ErrorResponse> handleUnprocessableEntityException(UnprocessableEntityException ex){
 
+        log.error("SE LANZÓ 'handleUnprocessableEntityException': {}", ex.getMessage());
+
         HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
 
         ErrorResponse response = ErrorResponse
@@ -79,71 +89,81 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(response, status);
     }
-        // Manejador específico para las posibles excepciones para ResourceNotFoundException
-        @ExceptionHandler(ResourceNotFoundException.class)
-        public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex){
-    
-            HttpStatus status = HttpStatus.NOT_FOUND;
-    
-            ErrorResponse response = ErrorResponse
-                    .builder()
-                    .status(status.value())
-                    .error("Recurso no encontrado")
-                    .message(ex.getMessage())
-                    .details(ex.getErrorDetails())
-                    .build();
-    
-            return new ResponseEntity<>(response, status);
-        }
-    
-        // Manejador para errores de validación de argumentos (@Valid)
-            @ExceptionHandler(MethodArgumentNotValidException.class)
-            public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-                HttpStatus status = HttpStatus.BAD_REQUEST;
-        
-                List<String> errors = ex.getBindingResult().getFieldErrors().stream()
-                        .map(error -> error.getField() + ": " + error.getDefaultMessage())
-                        .collect(java.util.stream.Collectors.toList());
-        
-                ErrorResponse response = ErrorResponse
-                        .builder()
-                        .status(status.value())
-                        .error("Error de Validación")
-                        .message("Uno o más campos de la solicitud no son válidos.")
-                        .details(errors)
-                        .build();
-        
-                return new ResponseEntity<>(response, status);
-            }
-        
-            // Manejador para cuando falta un archivo MultipartFile
-            @ExceptionHandler(MissingServletRequestPartException.class)
-            public ResponseEntity<ErrorResponse> handleMissingServletRequestPartException(MissingServletRequestPartException ex) {
-                HttpStatus status = HttpStatus.BAD_REQUEST;
-        
-                ErrorResponse response = ErrorResponse.builder()
-                        .status(status.value())
-                        .error("Archivo Requerido Faltante")
-                        .message("Se esperaba un archivo, pero no se encontró en la solicitud.")
-                        .details(List.of(ex.getMessage()))
-                        .build();
-        
-                return new ResponseEntity<>(response, status);
-            }
-        
-            // Manejar excepciones generales del servidor
-            @ExceptionHandler(Exception.class)
-            public ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex) {
-                HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+    // Manejador específico para las posibles excepciones para ResourceNotFoundException
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex){
 
-                ErrorResponse response = ErrorResponse
-                        .builder()
-                        .status(status.value())
-                        .error("Error Interno del Servidor")
-                        .message("Ocurrió un error inesperado.")
-                        .details(List.of(ex.getMessage()))
-                        .build();
+        log.error("SE LANZÓ 'handleResourceNotFoundException': {}", ex.getMessage());
 
+        HttpStatus status = HttpStatus.NOT_FOUND;
+    
+        ErrorResponse response = ErrorResponse
+                .builder()
+                .status(status.value())
+                .error("Recurso no encontrado")
+                .message(ex.getMessage())
+                .details(ex.getErrorDetails())
+                .build();
+    
+        return new ResponseEntity<>(response, status);
+    }
+    
+    // Manejador para errores de validación de argumentos (@Valid)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+        log.error("SE LANZÓ 'handleMethodArgumentNotValidException': {}", ex.getMessage());
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        
+        List<String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(java.util.stream.Collectors.toList());
+        
+        ErrorResponse response = ErrorResponse
+                .builder()
+                .status(status.value())
+                .error("Error de Validación")
+                .message("Uno o más campos de la solicitud no son válidos.")
+                .details(errors)
+                .build();
+        
+        return new ResponseEntity<>(response, status);
+    }
+        
+    // Manejador para cuando falta un archivo MultipartFile
+    @ExceptionHandler(MissingServletRequestPartException.class)
+    public ResponseEntity<ErrorResponse> handleMissingServletRequestPartException(MissingServletRequestPartException ex) {
+
+        log.error("SE LANZÓ 'handleMissingServletRequestPartException': {}", ex.getMessage());
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        
+        ErrorResponse response = ErrorResponse.builder()
+                .status(status.value())
+                .error("Archivo Requerido Faltante")
+                .message("Se esperaba un archivo, pero no se encontró en la solicitud.")
+                .details(List.of(ex.getMessage()))
+                .build();
+        
+        return new ResponseEntity<>(response, status);
+    }
+        
+    // Manejar excepciones generales del servidor
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex) {
+
+        log.error("SE LANZÓ 'handleAllExceptions': {}", ex.getMessage());
+
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        ErrorResponse response = ErrorResponse
+                .builder()
+                .status(status.value())
+                .error("Error Interno del Servidor")
+                .message("Ocurrió un error inesperado.")
+                .details(List.of())
+                .build();
         return new ResponseEntity<>(response, status);
     }
 }
