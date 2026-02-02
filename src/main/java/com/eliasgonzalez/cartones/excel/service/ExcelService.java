acc3d7424c12c14a0096ff0a3e2eca8a90d6f39d
Elias Gonzalez <elias.gonzalez@ncoders.solutions>
2026-02-02 11:38:23 -0300

    mejorar manejo de excepciones en ExcelService.java con registro interno de errores detallado

--- src/main/java/com/eliasgonzalez/cartones/excel/service/ExcelService.java
+++ src/main/java/com/eliasgonzalez/cartones/excel/service/ExcelService.java
@@ -118,7 +118,8 @@ public class ExcelService implements IExcelService {
             }
 
         } catch (ExcelProcessingException | FileProcessingException e) {
-            throw e; // Relanzar para que el GlobalExceptionHandler lo maneje
+            log.error("[INTERNO] Fallo en el procesamiento del Excel. Errores: {}. Mensaje: {}", erroresGlobales,e.getMessage());
+            throw e; // Re-lanzar la excepción para que GlobalExceptionHandler la capture
         } catch (Exception e) {
             log.error("Fallo crítico en el procesamiento del Excel", e);
             throw new RuntimeException("Error al procesar el archivo Excel: " + e.getMessage(), e);
